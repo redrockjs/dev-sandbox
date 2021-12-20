bdd0d3b87da1867dbe62005fdfd714f08622eedc
redrockjs <smb.sochi@gmail.com>
2021-12-20 16:03:28 +0300

    fix: some fixex

--- create-js-app/webpack.dev.js
+++ create-js-app/webpack.dev.js
@@ -1,8 +1,9 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const path = require('path');
 
 const htmlFile = /^([-_\d\w]+).html$/i;
-const srcPath = path.resolve(__dirname, 'src');
+const srcPath = path.resolve(__dirname, '../src');
 
 module.exports = merge(common, {
   mode: 'development',
